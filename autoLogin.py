c21a691c47495d4c38045cc3d6d3e0d1e5ca4487
miaoyu <1337611267@qq.com>
2018-01-17 13:41:44 +0800

    init

---
+++ autoLogin.py
@@ -0,0 +1,11 @@
+# encoding urf-8
+# https://10.0.8.46:18443/sefon-cas/login?service=https%3A%2F%2F10.0.8.49%3A18443%2Fminer%2F&locale=zh_CN
+
+url = 'https://10.0.8.46:18443/sefon-cas/login?service=https%3A%2F%2F10.0.8.49%3A18443%2Fminer%2F&locale=zh_CN'
+
+password  = '221b339db1d6f6dfdc45737cfa20004d'
+lt        = 'LT-167-mbdeJBPAMGkrQaSJN2aekmBRZ3cfQi'
+execution =	'e2s1'
+_eventId  = 'submit'
+username  = 'fff'
+captcha   = 'xhua'
